502d452429fca66e498fd5d8d0aff223ebf58faf
Gabriel Souza <ga.ferreira.desouza@gmail.com>
2024-07-10 16:34:59 -0300

    implementação da camada de service

---
+++ bootcampSantanderJava/santander-dev-week-2023/src/main/java/edu/gsohz/santander_dw23/service/impl/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package edu.gsohz.santander_dw23.service.impl;
+
+import edu.gsohz.santander_dw23.domain.model.User;
+import edu.gsohz.santander_dw23.domain.repository.UserRepository;
+import edu.gsohz.santander_dw23.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This Account number already exists.");
+        }
+        return userRepository.save(userToCreate);
+    }
+}
